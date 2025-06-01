e988752eabcd9a0979d2f3637fee52a30c692770
keithunt-35 <tugume keith022@gmail.com>
2025-06-01 17:03:36 +0300

    abstraction

--- morning sessions/oop.py
+++ morning sessions/oop.py
@@ -7,18 +7,15 @@
 
 #key principles
 
-#inheritance
-#super class: base class, parent class.
-#sub class: derived class, child class.
-
+#************inheritance*************
 #example 1: inheritence
 
-#super class
+#super class: base class, parent class.
 class Animal:
     def speak(self):
         print('Animal makes a sound')
 
-#sub class
+#sub class: derived class, child class.
 class Cat (Animal):
     def sound(self):
         print('cat makes a souund meoeeewww')
@@ -28,17 +25,87 @@ cat1 = Cat()
 
 #callmg the inherited method
 cat1.speak()
+#calling its own method
+cat1.sound()
 
 
-#polymorphism 
 
+
+#******************polymorphism*****************
+#allows objects of diff classes behave like they are from one class 
+
+#parent
+class Bird():
+    def fly(self):
+        print("birds fly in the sky")
+        
+class Eagle(Bird):
+    def fly(self):
+        print("eagle flies at a very hight altitude")
+        
+class Sparrow(Bird):
+    def fly(self):
+        print('Saprrow flies at a low altitube')
+        
+        
+#how we use polymorphism
+def flight_test(Bird):
+    Bird.fly() #run respecrive class methid based in an object
+    
+    
+#crearint the objects
+eagle1 = Eagle()
+sparrow1 = Sparrow()
+
+#calling the flight test method with different objects
+
+flight_test(eagle1)
 
 #method overriding and method over loading
 
 
 
-#abstraction
+#************abstraction*****************
+# aclass that contains one or more methods
 
+#class and interfaces
+#uses the symbol @ 
+#import the ABc anf the abstract method formthe abc module
+from abc import ABC, abstractmethod #abstraction
+
+#starting an car engine and a bike
+
+#define an abstract class
+
+class Vehicle(ABC):
+    @abstractmethod
+    def start(self):
+        pass # this method,start, has no implementatin
+
+#child cass implementin the abstract method
+class Car(Vehicle):
+    def start(self):
+        print('car engine started')
+        
+        
+#derived class implementing an abstract method
+class Bike(Vehicle):
+    def start(self):
+        print("bike engine starts")
+
+#notes
+#we canot create object of an abstracr class
+#demo live
+#vehicle1 = Vehicle() #this would raise an error cannot instantialte an abstract class vehicle without an  implementation for the abstracr method start
+
+#the accepted version is:: create object of the subclass
+
+car1 = Car()
+bike1 = Bike()
+
+#display output if the methods
+car1.start()
+bike1.start()
 
 
 #encapsulation
